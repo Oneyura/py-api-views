570c8ce2bde96b3265e2d4a792c71ce6a8265e75
Oneyura <hryn.yuri@gmail.com>
2025-04-27 17:56:14 +0300

    fix views

--- cinema/urls.py
+++ cinema/urls.py
@@ -10,9 +10,19 @@ from cinema.views import (
     MovieViewSet
 )
 
+cinema_hall_list = CinemaHallViewSet.as_view({
+    "get": "list",
+    "post": "create",
+})
+cinema_hall_detail = CinemaHallViewSet.as_view({
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
+
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
-router.register("cinema-hall", CinemaHallViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
@@ -20,6 +30,8 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema-halls/", cinema_hall_list, name="cinemahall-list"),
+    path("cinema-halls/<int:pk>/", cinema_hall_detail, name="cinemahall-detail"),
 ]
 
 app_name = "cinema"
