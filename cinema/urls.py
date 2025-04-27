d44775e418b0877010440f83eb24ad86218dd323
Oneyura <hryn.yuri@gmail.com>
2025-04-27 17:13:18 +0300

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,38 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    ActorList,
+    ActorDetail,
+    GenreList,
+    GenreDetail,
+    CinemaViewSet,
+    MovieViewSet
+)
 
+movie_list = MovieViewSet.as_view({"get": "list", "post": "create"})
+movie_detail = MovieViewSet.as_view({
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"
+})
+
+cinema_list = CinemaViewSet.as_view({"get": "list", "post": "create"})
+cinema_detail = CinemaViewSet.as_view({
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"
+})
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("cinema-halls/", cinema_list, name="cinema-list"),
+    path("cinema-halls/<int:pk>/", cinema_detail, name="cinema-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
 ]
 
 app_name = "cinema"
