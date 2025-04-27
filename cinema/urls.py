f0f6df8e2e0d28a568fd0e9e60b22e45890d5b0d
Oneyura <hryn.yuri@gmail.com>
2025-04-27 18:01:10 +0300

    fix urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -30,11 +30,11 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema-halls/", cinema_hall_list, name="cinemahall-list"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinema-halls/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
-        name="cinemahall-detail"
+        name="cinema-hall-detail"
     ),
 ]
 
