3c47f2473a54d5e3e74a7043c576de581305cc5e
Oneyura <hryn.yuri@gmail.com>
2025-04-27 17:29:09 +0300

    add router

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from cinema.views import (
     ActorList,
@@ -8,6 +9,10 @@ from cinema.views import (
     CinemaHallViewSet,
     MovieViewSet
 )
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("cinema-halls", CinemaHallViewSet)
 
 movie_list = MovieViewSet.as_view({"get": "list", "post": "create"})
 movie_detail = MovieViewSet.as_view({
@@ -25,12 +30,9 @@ cinema_detail = CinemaHallViewSet.as_view({
     "delete": "destroy"
 })
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("cinema-halls/", cinema_list, name="cinema-list"),
-    path("cinema-halls/<int:pk>/", cinema_detail, name="cinema-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
 ]
